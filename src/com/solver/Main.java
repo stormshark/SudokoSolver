b498ecb219f1eafda714f0c2bc90e94e87bd1893
stormshark_laptop <Stormshark312@hotmail.com>
2020-10-07 00:20:15 +0300

    adım 0

---
+++ src/com/solver/Main.java
@@ -0,0 +1,19 @@
+package com.solver;
+
+public class Main {
+
+    public static void main(String[] args) {
+    int sudokoDimension;
+
+
+    }
+
+    public boolean checkSudokoDimension (int dimension) {
+        if ((dimension <= 3) && (dimension >= 2)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
